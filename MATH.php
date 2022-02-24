c169427c381f3bc3b89d540b39eba30106eb65b1
Chrizel192 <98368481+Chrizel192@users.noreply.github.com>
2022-02-24 15:39:36 +0800

    Update MATH.php

--- MATH.php
+++ MATH.php
@@ -3,6 +3,13 @@
 <title>MATH</title>
 </head>
 <body align="center" bgcolor="#bbbbbb">
-<img src="MATH.png" height="50%" width="90%">
+<img src="MATH.png" height="40%" width="80%">
+<p>
+<img src="MATH PT.png" height="90%" width="60%">
+<p>
+<Font size="19">CLICK THIS LINK TO SEE THE VIDEO</font>
+<p>
+<img src="Arrow.png">
+<p>
+<font size="19"><a href="https://youtu.be/fIvVtg2YsC8">MESSAGE OF HOPE</a></font>
 </body>
-</html>
