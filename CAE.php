43c5abe03ddaaaf1a7ea8cdeac37bd60809a547f
Chrizel192 <98368481+Chrizel192@users.noreply.github.com>
2022-02-24 18:35:06 +0800

    Update CAE.php

--- CAE.php
+++ CAE.php
@@ -3,6 +3,24 @@
 <title>CAE</title>
 </head>
 <body align="center" bgcolor="#bd9c81">
-<img src="CAE.png" height="50%" width="90%">
+<img src="CAE.png" height="40%" width="80%">
+<p>
+<img src="1st page.png" heigt="90%" width="80%">
+<p>
+<img src="2nd page.png" heigt="90%" width="80%">
+<p>
+<img src="3rd page.png" heigt="90%" width="80%">
+<p>
+<img src="4th page.png" heigt="90%" width="80%">
+<p>
+<img src="5th page.png" heigt="90%" width="80%">
+<p>
+<img src="6th page.png" heigt="90%" width="80%">
+<p>
+<img src="7th page.png" heigt="90%" width="80%">
+<p>
+<img src="8th page.png" heigt="90%" width="80%">
+<p>
+<img src="9th page.png" heigt="90%" width="80%">
 </body>
 </html>
