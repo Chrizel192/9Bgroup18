c622019f85866f77acceeefd8c94c9c11a782f84
Chrizel192 <98368481+Chrizel192@users.noreply.github.com>
2022-02-24 23:54:14 +0800

    Update CAE.php

--- CAE.php
+++ CAE.php
@@ -5,22 +5,26 @@
 <body align="center" bgcolor="#bd9c81">
 <img src="CAE.png" height="40%" width="80%">
 <p>
-<img src="1st page.png" heigt="90%" width="80%">
+<img src="1.png" heigt="90%" width="80%">
 <p>
-<img src="2nd page.png" heigt="90%" width="80%">
+<img src="2.png" heigt="90%" width="80%">
 <p>
-<img src="3rd page.png" heigt="90%" width="80%">
+<img src="3.png" heigt="90%" width="80%">
 <p>
-<img src="4th page.png" heigt="90%" width="80%">
+<img src="4.png" heigt="90%" width="80%">
 <p>
-<img src="5th page.png" heigt="90%" width="80%">
+<img src="5.png" heigt="90%" width="80%">
 <p>
-<img src="6th page.png" heigt="90%" width="80%">
+<img src="6.png" heigt="90%" width="80%">
 <p>
-<img src="7th page.png" heigt="90%" width="80%">
+<img src="7.png" heigt="90%" width="80%">
 <p>
-<img src="8th page.png" heigt="90%" width="80%">
+<img src="8.png" heigt="90%" width="80%">
 <p>
-<img src="9th page.png" heigt="90%" width="80%">
+<img src="9.png" heigt="90%" width="80%">
+<p>
+<img src="10.png" heigt="90%" width="80%">
+<p>
+<img src="11.png" heigt="90%" width="80%">
 </body>
 </html>
