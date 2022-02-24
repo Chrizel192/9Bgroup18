126b534c9ab23140da5a2295d5864e92e39938e6
Chrizel192 <98368481+Chrizel192@users.noreply.github.com>
2022-02-24 17:22:17 +0800

    Update TLE.php

--- TLE.php
+++ TLE.php
@@ -2,7 +2,9 @@
 <head>
 <title>TLE</title>
 </head>
-<body align="center" bgcolor="#f5dcf9">
-<img src="TLE.png" height="50%" width="90%">
+<body align="center" bgcolor="#e9b0f2">
+<img src="TLE.png" height="40%" width="80%">
+<p>
+<img src="criteria.png" height="90%" width="80%">
 </body>
 </html>
