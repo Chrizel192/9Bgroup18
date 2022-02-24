e19cf58ada6c085b973ab2615b47885eb9085521
Chrizel192 <98368481+Chrizel192@users.noreply.github.com>
2022-02-24 16:01:13 +0800

    Update CLE.php

--- CLE.php
+++ CLE.php
@@ -3,6 +3,22 @@
 <title>CLE</title>
 </head>
 <body align="center" bgcolor="f1e1c4">
-<img src="CLE.png" height="50%" width="90%">
+<img src="CLE.png" height="40%" width="80%">
+<p>
+<img src="page 1.png" heigt="90%" width="60%">
+<p>
+<img src="page 2.png" heigt="90%" width="60%">
+<p>
+<img src="page 3.png" heigt="90%" width="60%">
+<p>
+<img src="page 4.png" heigt="90%" width="60%">
+<p>
+<img src="page 5.png" heigt="90%" width="60%">
+<p>
+<img src="page 6.png" heigt="90%" width="60%">
+<p>
+<img src="page 7.png" heigt="90%" width="60%">
+<p>
+<img src="page 8.png" heigt="90%" width="60%">
 </body>
 </html>
